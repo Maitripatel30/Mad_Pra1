58fefc2ab5711e016f6ee95ba81e266740abc0ab
Maitri Patel <maitripatel.301107@gmail.com>
2026-06-23 20:44:57 +0530

    commit

---
+++ src/p11.kt
@@ -0,0 +1,25 @@
+fun main(){
+    var j:Int=20
+    var k:Float=1.25f
+    var a:Double=2.5
+    var b:Long=12345678
+    var c:Char='M'
+    var d:Boolean=true
+    var f:String="Maitri"
+    var g:Short=2
+    var h:Byte=10
+    val x=100
+    j=200
+    println("x="+x)
+    println("Integer Value ="+j)
+    println("Float Value ="+k)
+    println("Double Value ="+a)
+    println("Long Value ="+b)
+    println("Char Value ="+c)
+    println("Boolean Value ="+d)
+    println("String Value ="+f)
+    println("Short Value ="+g)
+    println("Byte Value ="+h)
+    println("int= $j")
+    println(String.format("My name is %s",f))
+}
---
+++ src/p12.kt
@@ -0,0 +1,12 @@
+fun main(){
+    var i:Int=10
+    var j:Double=i.toDouble()
+    var s:String="1000"
+    var a:Int=s.toInt()
+    var b:Double=s.toDouble()
+    println(j)
+    println(a)
+    println(b)
+    println(i)
+    println(s)
+}
